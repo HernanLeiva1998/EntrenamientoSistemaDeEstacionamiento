dc50e16e2a7c4ebfca700c995dab6991b21cf737
Hernan <hernanleiva1998@gmail.com>
2023-06-22 10:52:30 -0300

    config db, agregado tags a junit

--- SistemaDeEstacionamiento/src/test/java/cespi/unlp/edu/ar/SistemaDeEstacionamiento/SistemaDeEstacionamientoApplicationTests.java
+++ SistemaDeEstacionamiento/src/test/java/cespi/unlp/edu/ar/SistemaDeEstacionamiento/SistemaDeEstacionamientoApplicationTests.java
@@ -2,12 +2,19 @@ package cespi.unlp.edu.ar.SistemaDeEstacionamiento;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
+@Transactional
+@Rollback(true)
 class SistemaDeEstacionamientoApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+	@Test
+	void initialTest() {
 	}
 
 }
