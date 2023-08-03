9d5940c1164caf53c6c6120f76767371443974da
Hernan <129122808+HernanLeiva1998@users.noreply.github.com>
2023-08-03 12:19:40 -0300

    exception handler y controller advice

---
+++ SistemaDeEstacionamiento/src/main/java/cespi/unlp/edu/ar/SistemaDeEstacionamiento/exceptions/ApiExceptionHandler.java
@@ -0,0 +1,17 @@
+packcage cespi.unlp.edu.ar.SistemaDeEstacionamiento.exceptions;
+
+import org.springframework.http.ResponseEntity;
+@ControllerAdvice
+public class ApiExceptionHandler {
+
+    
+    @ExceptionHandler(value = {SistemaDeEstacionamientoException.class})
+    public ResponseEntity<Object> handleApiRequestException(SistemaDeEstacionamientoException e){
+        //crear payload
+
+        //return response entity
+        if (e.getHttpStatus() != null) return ResponseEntity.status(e.getHttpStatus()).body(new ErrorDTO(e.getMessage()));
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorDTO(e.getMessage()));
+    }
+}
+
