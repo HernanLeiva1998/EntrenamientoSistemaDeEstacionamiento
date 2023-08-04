50ff6de2a14d043d72e9c1f087fbb7a5524c82c2
Hernan <hernanleiva1998@gmail.com>
2023-08-04 11:10:17 -0300

    merge

--- SistemaDeEstacionamiento/src/main/java/cespi/unlp/edu/ar/SistemaDeEstacionamiento/controllers/LoginController.java
+++ SistemaDeEstacionamiento/src/main/java/cespi/unlp/edu/ar/SistemaDeEstacionamiento/controllers/LoginController.java
@@ -23,21 +23,12 @@ public class LoginController {
 	
 	@PostMapping("/login")
 	   @CrossOrigin(origins = "http://localhost:4200")
-<<<<<<< HEAD
 	    public ResponseEntity<?> agregarPatenteAutomovilista(@RequestBody LoginDTO request) throws SistemaDeEstacionamientoException {
 		
 	            Automovilista automovilista = service.autenticar(request.getTelefono(), request.getContrasena());
 	  
 	            return ResponseEntity.ok().body(automovilista);
-=======
-	    public ResponseEntity<?> agregarPatenteAutomovilista(@RequestBody Map<String, String> request) throws SistemaDeEstacionamientoException {
-	        String password = request.get("contrasena");
-	        String telefono = request.get("telefono");
-	        
-	        Automovilista automovilista = service.autenticar(telefono, password);
-	            
-	        return ResponseEntity.ok().body(automovilista);
->>>>>>> 808c66d00b8631376604434b49c6fbf9609bc6d9
+
 	        
 	    }
 }
